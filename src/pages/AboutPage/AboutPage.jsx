09100dd74fe1be5ffc6260332a13abbae333c803
K1neticz <ejoliver@ymail.com>
2022-06-06 04:15:04 -0700

    Adds temporary About text

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,6 +2,16 @@ export default function AboutPage() {
     return (
         <div>
             <h1>About SEI-321</h1>
+            <p>apolgy for bad english 
+            <br></br>
+            where were u wen club penguin die
+            <br></br>
+            i was at house eating dorito when phone ring
+            <br></br>
+            "Club penguin is kil"
+            <br></br>
+            "no" 
+            </p>
         </div>
     );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div>
         <h1>Welcome To SEI-Goodbye</h1>
         <h3>The browser to keep in touch with students from General Assembly's SEIR-321!</h3>
-        
+        <button><a href="/about">About Our Cohort</a></button>
         </div>
     );
   }
