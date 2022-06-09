fb5bab7a4001cb06a0ac103d52b3bd99dbf2ffe7
K1neticz <ejoliver@ymail.com>
2022-06-09 03:18:39 -0700

    adds Css to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,10 +2,12 @@ import './HomePage.css'
 
 export default function Home() {
     return (
-        <div id="intro">
+        <div id="intro" className="box animate fadeInUp one">
+        <section>
         <h1>Welcome To SEI-Goodbye</h1>
-        <h3>The browser to keep in touch with students from General Assembly's SEIR-321!</h3>
-        <button><a href="/about">About Our Cohort</a></button>
+        <h3>The browser to keep in touch with <br></br> students from General Assembly's SEIR-321!</h3>
+        </section>
+        <button id="aboutBtn"><a href="/about">About Our Cohort</a></button>
         </div>
     );
   }
