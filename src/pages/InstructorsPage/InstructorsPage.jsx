887a975f98e6bd4b4158e0a27bb24261149afd78
K1neticz <ejoliver@ymail.com>
2022-06-13 10:01:42 -0700

    updates instructors page

--- src/pages/InstructorsPage/InstructorsPage.jsx
+++ src/pages/InstructorsPage/InstructorsPage.jsx
@@ -1,6 +1,6 @@
 import '../StudentsPage/DisplayPage.css';
 import { Link } from 'react-router-dom';
-export default function InstructorsPage({people}) {
+export default function InstructorsPage({people, getProfileData}) {
   const instructors = people.filter(p => p.category === 'instructor')
   const allInstructors = instructors.map(e => <p key= {e._id}>{e.name}</p>)
   console.log(instructors)
@@ -8,9 +8,11 @@ export default function InstructorsPage({people}) {
   return (
     <>
     <div id='list-stuff' className="box animate fadeInUp one">
-      <h1>Instructors</h1>
-      {instructors.map(e => <Link to = {`/profile/${e._id}`}>{e.name}</Link> )}
-      </div>
+    <h1>Instructors</h1>
+    {/* <Link to = {`/profile/${people._id}`}> */}
+      {instructors.map(e => <Link to={`/profile/${e._id}`}><button onClick={() => getProfileData(e._id)}>{e.name}</button><br></br></Link>)}
+      {/* // </Link> */}
+    </div>
     </>
   );
 }
